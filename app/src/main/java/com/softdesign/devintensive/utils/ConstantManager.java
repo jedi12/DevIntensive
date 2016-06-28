c87bcf4c14b144fb3e4a07500c4100031097da99
jedi12 <jedi12@mail.ru>
2016-06-28 21:53:30 +0700

    Activity life cycle is logged. Contacts activity layout created.

--- app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
+++ app/src/main/java/com/softdesign/devintensive/utils/ConstantManager.java
@@ -2,4 +2,11 @@ package com.softdesign.devintensive.utils;
 
 public interface ConstantManager {
     String TAG_PREFIX = "DEV ";
+    String EDIT_MODE_KEY = "EDIT_MODE_KEY";
+
+    String EDIT_PHONE_KEY = "EDIT_PHONE_KEY";
+    String EDIT_MAIL_KEY = "EDIT_MAIL_KEY";
+    String EDIT_VK_KEY = "EDIT_VK_KEY";
+    String EDIT_GIT_KEY = "EDIT_GIT_KEY";
+    String EDIT_BIO_KEY = "EDIT_BIO_KEY";
 }
